948ceb9e7ece3dfe7bbb1966e19d5ac2f418a0b7
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-05-14 22:53:26 +0200

    Get all Categories with a new service and controller

--- ComputerSpace/Server/Program.cs
+++ ComputerSpace/Server/Program.cs
@@ -2,6 +2,7 @@ global using ComputerSpace.Shared;
 global using ComputerSpace.Server.Data;
 global using Microsoft.EntityFrameworkCore;
 global using ComputerSpace.Server.Services.ProductService;
+global using ComputerSpace.Server.Services.CategoryService;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
