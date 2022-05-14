c4c958a0631455bbe2449a322461bc466d703d48
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-05-15 00:20:09 +0200

    Add category service on the client

--- ComputerSpace/Client/Program.cs
+++ ComputerSpace/Client/Program.cs
@@ -1,6 +1,7 @@
 global using ComputerSpace.Shared;
 global using System.Net.Http.Json;
 global using ComputerSpace.Client.Services.ProductService;
+global using ComputerSpace.Client.Services.CategoryService;
 using ComputerSpace.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,6 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
-
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
