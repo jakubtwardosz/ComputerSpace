9dc6ec7ef7eb3d4f3c3fd32ea7c399fd9e5267c5
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-05-23 15:00:53 +0200

    Modify the Cart Servicies

--- ComputerSpace/Client/Services/CartService/CartService.cs
+++ ComputerSpace/Client/Services/CartService/CartService.cs
@@ -22,8 +22,18 @@ namespace ComputerSpace.Client.Services.CartService
             {
                 cart = new List<CartItem>();
             }
-            cart.Add(cartItem);
 
+            var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId &&
+                x.ProductTypeId == cartItem.ProductTypeId);
+            if (sameItem == null)
+            {
+                cart.Add(cartItem);
+            }
+            else
+            {
+                sameItem.Quantity += cartItem.Quantity;
+            }
+            
             await _localStorage.SetItemAsync("cart", cart);
             OnChange.Invoke();
         }
