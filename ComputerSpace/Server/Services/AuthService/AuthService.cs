9092ade29e1f3236c28ecc93a56e6ae9044ab554
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-05-26 19:50:27 +0200

    The rest of implementation class for AuthService

--- ComputerSpace/Server/Services/AuthService/AuthService.cs
+++ ComputerSpace/Server/Services/AuthService/AuthService.cs
@@ -2,5 +2,14 @@
 {
     public class AuthService : IAuthService
     {
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
