b49d389eb7a78e5df028d583e5c188ee50ce19c2
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-05-30 17:18:34 +0200

    Implement the User Registration on the Server and Add the AuthController

--- ComputerSpace/Server/Services/AuthService/AuthService.cs
+++ ComputerSpace/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,6 @@
-﻿namespace ComputerSpace.Server.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace ComputerSpace.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
@@ -9,9 +11,26 @@
             _context = context;
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int> 
+                { 
+                    Success = false, 
+                    Message = "User already exists." 
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Data = user.Id };
         }
 
         public async Task<bool> UserExists(string email)
@@ -23,6 +42,16 @@
             }
 
             return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
