e1e95d18750bc30c28610f374afd705cb4ecd4ed
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-06-07 15:38:27 +0200

    Change the CartItem and add it to the database

--- ComputerSpace/Shared/CartItem.cs
+++ ComputerSpace/Shared/CartItem.cs
@@ -8,9 +8,9 @@ namespace ComputerSpace.Shared
 {
     public class CartItem
     {
+        public int UserId { get; set; }
         public int ProductId { get; set; }
         public int ProductTypeId { get; set; }
         public int Quantity { get; set; } = 1;
-
     }
 }
