0774dc4089f332956bef7e6b504eb44a009ed6e8
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-06-16 11:11:44 +0200

    Implement the Address Logic on the Client

--- ComputerSpace/Client/Program.cs
+++ ComputerSpace/Client/Program.cs
@@ -4,6 +4,7 @@ global using ComputerSpace.Client.Services.ProductService;
 global using ComputerSpace.Client.Services.CategoryService;
 global using ComputerSpace.Client.Services.AuthService;
 global using ComputerSpace.Client.Services.OrderService;
+global using ComputerSpace.Client.Services.AddressService;
 global using Microsoft.AspNetCore.Components.Authorization;
 
 using ComputerSpace.Client;
@@ -24,6 +25,7 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
