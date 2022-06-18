079da1fcce4655fbe1ffa0ab783bb1287d5fb70f
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-06-18 11:28:03 +0200

    Get a Single Product for Administrators

--- ComputerSpace/Server/Services/ProductService/ProductService.cs
+++ ComputerSpace/Server/Services/ProductService/ProductService.cs
@@ -3,9 +3,12 @@
     public class ProductService : IProductService
     {
         private readonly DataContext _context;
-        public ProductService(DataContext context)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProductService(DataContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<ServiceResponse<List<Product>>> GetAdminProducts()
@@ -37,10 +40,24 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products
-                .Include(p => p.Variants.Where(v => v.Visible && !v.Deleted))
-                .ThenInclude(v => v.ProductType)
-                .FirstOrDefaultAsync(p => p.Id == productId && !p.Deleted && p.Visible);
+            Product product = null;
+
+            if(_httpContextAccessor.HttpContext.User.IsInRole("Admin"))
+            {
+                product = await _context.Products
+                    .Include(p => p.Variants.Where(v => !v.Deleted))
+                    .ThenInclude(v => v.ProductType)
+                    .FirstOrDefaultAsync(p => p.Id == productId && p.Deleted);
+
+            }
+            else
+            {
+                product = await _context.Products
+                    .Include(p => p.Variants.Where(v => v.Visible && !v.Deleted))
+                    .ThenInclude(v => v.ProductType)
+                    .FirstOrDefaultAsync(p => p.Id == productId && !p.Deleted && p.Visible);
+            }
+
             if (product == null)
             {
                 response.Success = false;
