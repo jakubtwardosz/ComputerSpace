abc6eb2120439d1e45eb97f8597c6bee7f834dc8
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-06-18 09:44:02 +0200

    Finish the ProductType Administration Page

--- ComputerSpace/Server/Services/ProductTypeService/ProductTypeService.cs
+++ ComputerSpace/Server/Services/ProductTypeService/ProductTypeService.cs
@@ -11,6 +11,7 @@
 
         public async Task<ServiceResponse<List<ProductType>>> AddProductTypes(ProductType productType)
         {
+            productType.Editing = productType.IsNew = false;
             _context.ProductTypes.Add(productType);
             await _context.SaveChangesAsync();
 
