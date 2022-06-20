ae622a9177da4aff048290a65fb86150fb128df4
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-06-20 12:16:25 +0200

    Install MudBlazor

--- ComputerSpace/Client/Program.cs
+++ ComputerSpace/Client/Program.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using ComputerSpace.Client.Services.CartService;
+using MudBlazor.Services;
+
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -20,6 +22,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
